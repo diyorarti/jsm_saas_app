2d87c017bc6af63bd7fc27db9e6ac23c6a7aaa24
diyorarti <diyorarti@gmail.com>
2025-06-04 17:48:39 +0500

    Ai connected

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,33 @@
+import CompanionCard from '@/components/CompanionCard';
+import Searchinput from '@/components/Searchinput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const Companions = () => {
+const CompanionLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic})
+
   return (
-    <div>Companions</div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1>Companion Library</h1>
+        <div className='flex gap-4'>
+          <Searchinput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className='flex flex-wrap gap-4 w-full max-md:justify-center'>
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+        ))}
+      </section>
+    </main>
   )
 }
 
-export default Companions
+export default CompanionLibrary
